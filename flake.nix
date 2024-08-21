d2ddfa91853cb8f76c5a689a265f0296021b5263
Ankur Sharma <ankurs4@cisco.com>
2024-08-21 21:39:04 +0200

    add nix flake and hello world projects

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based Java development environment";
+  nixConfig.bash-prompt-suffix = "\[nix\] ";
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      javaVersion = 22; # Change this value to update the whole stack
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      overlays.default =
+        final: prev: rec {
+          jdk = prev."jdk${toString javaVersion}";
+          maven = prev.maven.override { jre = jdk; };
+          gradle = prev.gradle.override { java = jdk; };
+        };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ jdk jq maven gradle gcc ncurses patchelf zlib ];
+        };
+      });
+    };
+}
